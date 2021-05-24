52377d542ae41bba58238bfc122a8247ea879d1d
vincehedric05 <82695796+vincehedric05@users.noreply.github.com>
2021-05-25 01:54:16 +0800

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -489,6 +489,22 @@ namespace tictactoe
                 button8.Enabled = false;
                 button9.Enabled = false;
             }
+
+            // draw
+            else
+            {
+                winner_label.Text = "Draw";
+
+                button1.Enabled = false;
+                button2.Enabled = false;
+                button3.Enabled = false;
+                button4.Enabled = false;
+                button5.Enabled = false;
+                button6.Enabled = false;
+                button7.Enabled = false;
+                button8.Enabled = false;
+                button9.Enabled = false;
+            }
         }
     }
 }
