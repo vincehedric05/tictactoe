fa7c1e30f7a0b2a16289f0aeccfb4a43490651d9
vincehedric05 <82695796+vincehedric05@users.noreply.github.com>
2021-05-25 00:46:15 +0800

    appearance

--- Form1.cs
+++ Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace tictactoe
 {
-    public partial class Form1 : Form
+    public partial class TicTacToe : Form
     {
-        public Form1()
+        public TicTacToe()
         {
             InitializeComponent();
         }
@@ -183,6 +183,38 @@ namespace tictactoe
                 counter = counter + 1;
                 button9.Enabled = false;
             }
+        }
+
+        private void reset_button(object sender, EventArgs e)
+        {
+            counter = 1;
+
+            button1.Text = " ";
+            button1.Enabled = true;
+
+            button2.Text = " ";
+            button2.Enabled = true;
+
+            button3.Text = " ";
+            button3.Enabled = true;
+
+            button4.Text = " ";
+            button4.Enabled = true;
+
+            button5.Text = " ";
+            button5.Enabled = true;
+
+            button6.Text = " ";
+            button6.Enabled = true;
+
+            button7.Text = " ";
+            button7.Enabled = true;
+
+            button8.Text = " ";
+            button8.Enabled = true;
+
+            button9.Text = " ";
+            button9.Enabled = true;
         }
     }
 }
